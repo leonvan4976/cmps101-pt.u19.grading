33bbd53a90fa2ddd888dbb91b7ecd4880d934472
Hou-I Lin <47459281+houi-lin@users.noreply.github.com>
2019-06-30 22:09:18 -0700

    Update unit test script

--- pa1/pa1-unit-check.sh
+++ pa1/pa1-unit-check.sh
@@ -27,9 +27,10 @@ echo ""
 gcc -c -std=c99 -Wall -g ModelListTest.c List.c
 gcc -o ModelListTest ModelListTest.o List.o
 
-timeout 5 valgrind --leak-check=full -v ./ModelListTest -v > ListTest-out.txt
+timeout 5 valgrind --leak-check=full -v ./ModelListTest -v > ListTest-out.txt 2> MemoryCheck.txt
 
 cat ListTest-out.txt
+cat MemoryCheck.txt | grep --after-context=8 -E 'SUMMARY|freed'
 
 rm -f *.o ModelListTest* Lex
 
